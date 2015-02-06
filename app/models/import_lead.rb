d7db86e15a0fbf133f2e53dfbe6e97f9184afc91
Jeffrey Forsyth <jforsyth@keyimage.com>
2015-02-06 16:06:35 -0500

    added email and mobile

--- app/models/import_lead.rb
+++ app/models/import_lead.rb
@@ -12,17 +12,17 @@ class ImportLead
   end
 
   # Sample Format
-  # "Source","First Name","Last Name","Gender","Company Name","Phone Number","Address","City",
+  # "Source","First Name","Last Name", "Email", "Gender","Company Name","Phone Number", "Mobile Number","Address","City",
   # "State","ZIP code","ZIP+4","database name","File Date","id","Production Date"
   def import
     CSV.foreach(@file.path, :converters => :all, :return_headers => false, :headers => :first_row) do |row|
-      source, first_name, last_name, _, company, phone, *address = *row.to_hash.values
+      source, first_name, last_name, email, _, company, phone, mobile_phone, *address = *row.to_hash.values
 
       street, city, state, zip, _ = *address
       address = Address.new(:street1 => street, :city => city, :state => state, :zipcode => zip)
 
       lead = Lead.new(:source => source, :first_name => first_name, :last_name => last_name,
-                      :company => company, :phone => phone)
+                      :email => email, :company => company, :phone => phone, :mobile=> mobile_phone)
 
       lead.first_name = "FILL ME" if lead.first_name.blank?
       lead.last_name = "FILL ME" if lead.last_name.blank?
